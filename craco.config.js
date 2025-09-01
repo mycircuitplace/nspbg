33069737229905c430c949a9e6bcfa483d2b4c7b
Circuit Place <mycircuitplace@gmail.com>
2025-09-01 13:54:22 -0400

    CRACO fixed

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   style: {
-    postcss: {
+    postcssOptions: {
       plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
       ],
     },
   },
-}
+};
+
